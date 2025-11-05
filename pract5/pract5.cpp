646c1d5bee88b830653ec3fb8a52f25f17637dc9
Рита <rita.dyndar@mail.ru>
2025-11-05 09:37:19 +0300

    полный код с функцией встраивания водяного знака

--- pract5/pract5.cpp
+++ pract5/pract5.cpp
@@ -34,11 +34,59 @@ bool copyFile(const string& from, const string& to) {
     return true;
 }
 
+void embedWatermarkToFile(const string& hostFile, const string& watermarkFile, const string& outputFile) {
+    if (!copyFile(hostFile, outputFile)) {
+        cout << "Ошибка копирования файла!" << endl;
+        return;
+    }
+
+    ofstream file(outputFile, ios::binary | ios::app);
+    ifstream wm(watermarkFile, ios::binary);
+
+    if (!file || !wm) {
+        cout << "Ошибка добавления водяного знака!" << endl;
+        return;
+    }
+
+    const char* marker = "WATERMARK_START";
+    file.write(marker, strlen(marker));
+
+    file << wm.rdbuf();
+
+    cout << "Водяной знак встроен в файл!" << endl;
+}
+
 int main() {
     setlocale(LC_ALL, "rus");
 
-    // Базовая структура программы
-    cout << "Программа для работы с водяными знаками" << endl;
+    cout << "Выберите основной файл (любой):" << endl;
+    string hostPath = OpenFileDialog();
+    if (hostPath.empty()) {
+        cout << "Файл не выбран! Программа завершена." << endl;
+        system("pause");
+        return 0;
+    }
+    cout << "Основной файл: " << hostPath << endl;
 
+    cout << "\nВыберите файл водяного знака (любой):" << endl;
+    string watermarkPath = OpenFileDialog();
+    if (watermarkPath.empty()) {
+        cout << "Файл не выбран! Программа завершена." << endl;
+        system("pause");
+        return 0;
+    }
+    cout << "Водяной знак: " << watermarkPath << endl;
+
+    // Создаем имя для выходного файла
+    string outputPath = "watermarked_" + hostPath.substr(hostPath.find_last_of("\\") + 1);
+
+    cout << "\nНачинаю встраивание..." << endl;
+
+    // Встраиваем водяной знак
+    embedWatermarkToFile(hostPath, watermarkPath, outputPath);
+
+    cout << "Водяной знак успешно добавлен в файл: " << outputPath << endl;
+    cout << "Нажмите любую клавишу для выхода..." << endl;
+    system("pause");
     return 0;
 }
